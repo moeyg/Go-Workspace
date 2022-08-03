1751cf7e31b755ce67b05b96ffc3831c4b95ee64
moeyg <moooeyg@gmail.com>
2022-08-03 10:30:00 +0900

    Add : type.go

---
+++ Note/02.Variable/type.go
@@ -0,0 +1,33 @@
+// Variable Type : 변수 타입
+
+package main
+
+// uint (unsigned integer) : 부호 없는 정수 = 양수
+// uint8, uint16, uint32, uint64
+
+// int (integer) : 정수
+// int8, int16, int32, int64
+
+// float : 실수
+// float32, float64
+
+// bool : true / false
+
+// string : 문자열
+
+// array : 배열
+
+// slice : 사이즈가 정해지지 않은 배열
+
+// structure
+
+// pointer
+
+// func
+
+// map
+
+// interface
+
+// rune = int32
+// byte = unit8
