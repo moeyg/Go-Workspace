4aa0ddcea092d264d2e745311c1242d2818cb89a
moeyg <moooeyg@gmail.com>
2022-08-07 00:55:00 +0900

    Add : input.go

---
+++ Note/03.fmt/input.go
@@ -0,0 +1,17 @@
+// Standard Input Function (표준 입력 함수)
+
+package main
+
+import "fmt"
+
+func main () {
+
+	// Scan() : 표준 입력에서 값을 입력
+	fmt.Scan()
+
+	// Scanln() : 표준 입력에서 한 줄을 읽어서 값을 입력
+	fmt.Scanln()
+
+	// Scanf() : 표준 입력에서 서식 형태로 값을 입력
+	fmt.Scanf()
+}
