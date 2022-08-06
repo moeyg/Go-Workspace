3eb6d5de6817a5b4ce557ce204bf5e36869f5bc1
moeyg <moooeyg@gmail.com>
2022-08-07 00:40:00 +0900

    Add : specialSymbols.go

---
+++ Note/03.fmt/specialSymbols.go
@@ -0,0 +1,21 @@
+// Special Symbols (특수문자)
+
+package main
+
+import "fmt"
+
+func specialSymbols () {
+	var integer int = 90
+
+	// \n : 줄바꿈
+	fmt.Printf("\\n : %d\n", integer) // \n : 90
+
+	// \t : 탭
+	fmt.Printf("\\t : \t%d\n", integer) // \t :    90
+
+	// \\ : \ 출력
+	fmt.Printf("\\\\ : \\%d\\\n", integer) // \\ : \90\
+
+	// \' \" : ', " 출력
+	fmt.Printf("\\\" : \"%d\"\n", integer) // \" : "90"
+}
