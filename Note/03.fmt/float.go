17b2e405d8963348670f357032ca26a17cab3b64
moeyg <moooeyg@gmail.com>
2022-08-07 00:30:00 +0900

    Add : float.go

---
+++ Note/03.fmt/float.go
@@ -0,0 +1,16 @@
+// Float Output (실수 출력) : %f, %g
+
+package main
+
+import "fmt"
+
+func float () {
+	var pi float64 = 3141592.65359
+	
+	// %f : 소수점 6자리까지 표현
+	fmt.Printf("f : %f\n", pi) // f : 3141592.653590
+
+	// %g(%v) : 길이(6자리)에 따라서 지수 표현 또는 실수 표현으로 출력
+	fmt.Printf("g : %g\n", pi) // g : 3.14159265359e+06
+	fmt.Printf("v : %v\n", pi) // v : 3.14159265359e+06
+}
