765c0e6ebdbe1e5e1055ab8a5fbb3e4fe6cd578a
moeyg <moooeyg@gmail.com>
2022-08-07 00:19:00 +0900

    Add : decimal.go

---
+++ Note/03.fmt/decimal.go
@@ -0,0 +1,18 @@
+// Decimal Output (정수 출력) : %d
+
+package main
+
+import "fmt"
+
+func decimal () {
+	var integer int = 90
+
+	// 5칸을 오른쪽 정렬
+	fmt.Printf("%5d\n", integer) //    90
+
+	// 5칸을 오른쪽 정렬 + 공백을 0으로
+	fmt.Printf("%05d\n", integer) // 00090
+
+	// 5칸을 왼쪽 정렬
+	fmt.Printf("%-5d", integer) // 90   
+}
