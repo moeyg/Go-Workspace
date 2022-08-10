2b4c880532816046ae70bf00a3d342b489bada71
moeyg <moooeyg@gmail.com>
2022-08-10 17:42:57 +0900

    Add : ex07.go

---
+++ Note/exercise/ex07.go
@@ -0,0 +1,23 @@
+// Using the following operators, write expressions and assign their values to variables:
+
+// 🅐 ==
+// 🅑 <=
+// 🅒 >=
+// 🅓 !=
+// 🅔 <
+// 🅕 >
+
+// Now print each of the variables
+
+package main
+
+import "fmt"
+
+func ex07() {
+	fmt.Println(90 == 90) // true
+	fmt.Println(90 <= 100) // true
+	fmt.Println(90 >= 100) // false
+	fmt.Println(90 != 90) // false
+	fmt.Println(90 < 100) // true
+	fmt.Println(90 > 100) // false
+}
