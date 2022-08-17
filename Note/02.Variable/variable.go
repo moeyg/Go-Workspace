b37aa637216ff127d85bbbf315d8657c6d9d359e
moeyg <moooeyg@gmail.com>
2022-08-17 16:32:00 +0900

    Chore : Add boolean data type

--- Note/02.Variable/variable.go
+++ Note/02.Variable/variable.go
@@ -6,10 +6,16 @@ import "fmt"
 
 func variable() {
 	// var = variable, 변수 선언
-	// int = 변수 타입, 정수 타입
+
+	// int = 정수 타입
 	var num int = 10
-	// sting = 뱐수 타입, 문자열 타입
+
+	// sting = 문자열 타입
 	var message string = "Hello World"
+
+	// bool = 불리언 타입
+	var truthy bool = true
+	var falsy bool = false
 
 	// 변수값 재할당
 	num = 90
@@ -17,4 +23,5 @@ func variable() {
 
 	// 변수값 출력
 	fmt.Println(message, num)
+	fmt.Println(truthy, falsy)
 }
