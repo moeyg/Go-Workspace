d62ea822c928074cff6d8a8282be0f05048b4adf
moeyg <moooeyg@gmail.com>
2022-08-20 17:46:50 +0900

    Add : initializer.go

---
+++ Note/09.Switch/initializer.go
@@ -0,0 +1,43 @@
+// Initializer : 초기문
+
+package main
+
+import "fmt"
+
+func isAge() (age int) {
+	fmt.Println("How old are you? :")
+	fmt.Scanf("%d", &age)
+	return age
+}
+
+func main() {
+	// switch 초기문; 비교값 { }
+	// 변수 age는 switch 내에서 사용하는 지역 변수 
+	switch age := isAge(); true {
+		case age < 0 :
+			fmt.Println("Please enter your correct age")
+
+		case 0 <= age && age < 5 :
+			fmt.Printf("%d years old - baby\n", age)
+			fmt.Println("FREE ENTRY")
+
+		case age < 10 :
+			fmt.Printf("%d years old - child\n", age)
+			fmt.Println("PAY $35")
+
+		case age < 20 :
+			fmt.Printf("%d years old - teenager\n", age)
+			fmt.Println("PAY $50")
+
+		case age < 60 :
+			fmt.Printf("%d years old - adult\n", age)
+			fmt.Println("PAY $60")
+		
+		case age >= 60:
+			fmt.Printf("%d years old, senior\n", age)
+			fmt.Println("FREE ENTRY")
+		
+		default :
+			fmt.Println("Please enter your correct age")
+	}
+}
